b6bbc378e261f6a29dbd161867a6640bc2b71941
jm.nissel <jm.nissel@5779d460-e0ed-11de-a668-6fd11605f5ce>
2009-12-09 21:52:49 +0000

    changed paddle acceleration.

--- trunk/ArkaDroid_Main/src/uk/co/coldasice/projects/android/arkadroid/sprites/SpritePaddle.java
+++ trunk/ArkaDroid_Main/src/uk/co/coldasice/projects/android/arkadroid/sprites/SpritePaddle.java
@@ -5,25 +5,17 @@ import android.graphics.drawable.Drawable;
 
 public class SpritePaddle extends Sprite_Trail {
 	
-	private static final int LASTPRESSED_LEFT = -1;
-	private static final int LASTPRESSED_RIGHT = 1;
-	private static final int LASTPRESSED_NONE = 0;
-	private static final double SPEED_DECEL = 0.1;
-	private static final double SPEED_ACCEL = 0.1;
+	private static final double SPEED_DECEL = 1.0;
+	private static final double SPEED_ACCEL = 0.5;
 	
-	private double currentSpeed = 0;
-	private double desiredSpeed = 0;
+	private double topSpeed = 4.0;
+	private double currentSpeedLeft = 0;
+	private double currentSpeedRight = 0;
 	public boolean rightPressed = false;
 	public boolean leftPressed = false;
-	private int lastPressed = LASTPRESSED_NONE;
 	
 	public SpritePaddle(Drawable drawable, GameRenderer renderer) {
 		super(drawable, renderer);
-	}
-	
-	public void setSpeed(double speed) {
-		desiredSpeed = speed;
-		// currentSpeed = 0;
 	}
 	
 	public void setRightPressed(boolean pressed) {
@@ -33,40 +25,48 @@ public class SpritePaddle extends Sprite_Trail {
 	public void setLeftPressed(boolean pressed) {
 		leftPressed = pressed;
 	}
+	
+	public void setSpeed(double speed){
+		topSpeed = speed;
+	}
 
 	public void update(double timediff) {
-		// accelerate the ball towards desired speed, if just one direction is pressed
-		if (leftPressed ^ rightPressed && desiredSpeed > currentSpeed) {
-			currentSpeed += (desiredSpeed - currentSpeed) * SPEED_ACCEL;
-		}
+
 		// left pressed?
 		if(leftPressed && !rightPressed){
-			setX(x - (currentSpeed*timediff));
-			lastPressed = LASTPRESSED_LEFT;
+			currentSpeedLeft = adjustPressedSpeed(currentSpeedLeft);
+			currentSpeedRight = Math.max(0, currentSpeedRight-SPEED_DECEL);
 		}
 		// right pressed?
 		else if(rightPressed && !leftPressed){
-			setX(x + (currentSpeed*timediff));
-			lastPressed = LASTPRESSED_RIGHT;
+			currentSpeedRight = adjustPressedSpeed(currentSpeedRight);
+			currentSpeedLeft = Math.max(0, currentSpeedLeft-SPEED_DECEL);
 		}
 		// nothing pressed, or both pressed? decelerate the ball towards 0,
 		// the move the ball at the new speed in the original direction
 		// it was going
 		else  {
 			// slow down the paddle 
-			if (currentSpeed > 0) {
-				currentSpeed -= currentSpeed * SPEED_DECEL;
-			}
-			setX(x + lastPressed * (currentSpeed*timediff));
+			currentSpeedLeft = Math.max(0, currentSpeedLeft-SPEED_DECEL);
+			currentSpeedRight = Math.max(0, currentSpeedRight-SPEED_DECEL);
 		}
+		setX(x + ((currentSpeedRight-currentSpeedLeft)*timediff));
+	}
+	
+	private double adjustPressedSpeed(double currentSpeed){
+		if (currentSpeed < topSpeed) {
+			return Math.min(topSpeed,currentSpeed+SPEED_ACCEL);
+		}
+		if(currentSpeed > topSpeed) return Math.max(0,currentSpeed-SPEED_DECEL);
+		return currentSpeed;
+		
 	}
 	
 	public void reset() {
 		super.reset();
-		lastPressed = LASTPRESSED_NONE;
 	}
 
 	public double getPaddleSpeed() {
-		return currentSpeed;
+		return currentSpeedRight-currentSpeedLeft;
 	}
 }
